49cd5ec7155b2ee4cbc911d8e8909593fcb03800
IvanVulovic <ivn.vulovic@gmail.com>
2022-08-06 19:53:38 +0200

    test

---
+++ src/app/controllers/app/index.ts
@@ -0,0 +1,81 @@
+import { useContext } from 'react';
+
+import useAppImplementation from '@implementation/app';
+import useWishlistImplementation from '@implementation/wishlist';
+import { useDispatch } from '@service';
+
+import AuthContext from './context';
+import scopeSettings, { AppsScope } from './settings';
+
+const useAppController = (scope) => {
+  const dispatch = useDispatch();
+  const app = useAppImplementation();
+  const getPayload = (params: any = null) => ({
+    controller: scopeSettings[scope],
+    params,
+  });
+  const onCreate = (params: any) => {
+    const payload = getPayload(params);
+    dispatch(app.actions.create(payload));
+  };
+  const onLoadAll = (params: any) => {
+    const payload = getPayload(params);
+    dispatch(app.actions.loadAll(payload));
+  };
+  // const onLoadOne = (params: any) => {
+  //   const payload = getPayload(params);
+  //   dispatch(app.actions.loadOne(payload));
+  // };
+  const onRemove = (params: any) => {
+    const payload = getPayload(params);
+    dispatch(app.actions.remove(payload));
+  };
+
+  return {
+    controller: scopeSettings[scope],
+    implementation: {
+      app,
+    },
+    methods: {
+      onCreate,
+      onLoadAll,
+      // onLoadOne,
+      onRemove,
+    },
+  };
+};
+
+export const useWishlistController = () => {
+  const dispatch = useDispatch();
+  const scope = useAppController(AppsScope.Wishlist);
+  const wishlist = useWishlistImplementation();
+
+  const getPayload = (params: any = null) => ({
+    controller: scopeSettings[AppsScope.Wishlist],
+    appScope: scope,
+    params,
+  });
+
+  const onFetch = (params: any) => {
+    const payload = getPayload(params);
+    dispatch(wishlist.actions.fetch(payload));
+  };
+
+  scope.implementation.wishlist = wishlist;
+  scope.methods.onFetch = onFetch;
+
+  return scope;
+};
+
+const useAppControllerScope = () => {
+  const ctx = useContext(AuthContext);
+  if (!ctx) {
+    throw new Error('You are using AppContext out of AppContext.Provider');
+  }
+  return ctx;
+};
+
+export const useWishlistControllerScope = () => {
+  const appScope = useAppControllerScope();
+  return appScope;
+};
